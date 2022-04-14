7e861d9f8608b3197b8ad5af7ad4c577dd6d7333
Waoweens <alikooshafarzan@gmail.com>
2022-04-14 11:08:31 +0700

    Initial Mod Commit

---
+++ src/main/java/net/meowcorp/dynamicip/getip_fabric/GetIP.java
@@ -0,0 +1,11 @@
+package net.meowcorp.dynamicip.getip_fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GetIP implements ModInitializer {
+    public static String MODID = "getip";
+    @Override
+    public void onInitialize() {
+
+    }
+}
